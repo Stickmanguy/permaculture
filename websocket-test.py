c13eead99c2b36643abc7acf62c04acf40126482
Stickmanguy <47448205+Stickmanguy@users.noreply.github.com>
2019-02-09 20:49:58 +0100

    Add files via upload

---
+++ websocket-test.py
@@ -0,0 +1,19 @@
+#usr/bin/python
+
+import websocket
+import time
+ 
+ws = websocket.WebSocket()
+ws.connect("ws://192.168.1.50/")
+ 
+i = 0
+nrOfMessages = 30
+ 
+while i<nrOfMessages:
+    ws.send("Soft AP mode: message nr " + str(i))
+    result = ws.recv()
+    print(result)
+    i=i+1
+    time.sleep(1)
+ 
+ws.close()
